2e7f191ae1f069db956ddbf163f10ee548ed5748
Kirill <kirbo06@mail.ru>
2022-02-13 20:52:58 +0100

    - v0.4 release

---
+++ release/v0.4.js
@@ -0,0 +1,9 @@
+MODULE({
+  "name": "hello-world",
+  "code_version": 1,
+  "executable": "helloWorld"
+});
+//file src/src.js
+function helloWorld() {
+    java.lang.System.out.println("Hello, World!");
+}
